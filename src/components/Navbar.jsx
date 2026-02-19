1427bc1757682d73568fd001ea54ca4a22cdfcb5
benedicta-abragam <benedictaabraham079@gmail.com>
2026-02-19 14:11:59 +0530

    Updated mobile menu spacing and improved NavLink structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import "../styles/Navbar.css"
 import { NavLink } from "react-router-dom"
 
 function Navbar() {
-  const [open, setopen] = useState(false)
+  const [open, setOpen] = useState(false)
 
   const handleClose =()=>{
-    setopen(false)
+    setOpen(false)
   }
   return (
-    <nav className=" fixed top-0 w-full h-[50px] flex justify-between items-center px-20 bg-[#050f14]">
+<nav className="fixed top-0 w-full h-[60px] flex justify-between items-center px-6 md:px-20 bg-[#050f14] z-50">
       
       <div className="font-semibold neon-text">
         Benedicta A
@@ -17,32 +17,36 @@ function Navbar() {
 
       {/* Desktop menu */}
       <ul className="hidden md:flex gap-8"> 
-        <li className="nav-link"><NavLink to="/">Home</NavLink></li>
-        <li className="nav-link"><NavLink to="/about">About</NavLink></li>
-        <li className="nav-link"><NavLink to="/skills">Skills</NavLink></li>
-        <li className="nav-link"><NavLink to="/projects">Projects</NavLink></li>
-        <li className="nav-link"><NavLink to="/contact">Contact</NavLink></li>
+        <li><NavLink  className="nav-link" to="/">Home</NavLink></li>
+        <li ><NavLink className="nav-link" to="/about">About</NavLink></li>
+        <li><NavLink  className="nav-link" to="/skills">Skills</NavLink></li>
+        <li ><NavLink  className="nav-link" to="/projects">Projects</NavLink></li>
+        <li ><NavLink  className="nav-link" to="/contact">Contact</NavLink></li>
       </ul>
 
       {/* Mobile icon */}
-      <div className="md:hidden text-2xl cursor-pointer" onClick={() => setopen(true)}>
+    <div className="md:hidden flex items-center text-2xl cursor-pointer" onClick={() => setOpen(true)}>
+
         <i className="fa-solid fa-bars text-[#00ffd5] icon"></i>
       </div>
 
-      {/* Mobile menu */}
-      <div className={`mobile-menu ${open ? "open" : ""}`}>
-        <div className="close-icon" onClick={handleClose}>
-          <i className="fa-solid fa-xmark icon"></i>
-        </div>
+    {/* Mobile menu */}
+<div className={`mobile-overlay ${open ? "show" : ""}`} onClick={handleClose}></div>
 
-        <ul className="flex flex-col gap-6">
-          <li className="nav-link"><NavLink to="/" onClick={() => setopen(false)}>Home</NavLink></li>
-          <li className="nav-link"><NavLink to="/about" onClick={() => setopen(false)}>About</NavLink></li>
-          <li className="nav-link"><NavLink to="/skills" onClick={() => setopen(false)}>Skills</NavLink></li>
-          <li className="nav-link"><NavLink to="/projects" onClick={() => setopen(false)}>Projects</NavLink></li>
-          <li className="nav-link"><NavLink to="/contact" onClick={() => setopen(false)}>Contact</NavLink></li>
-        </ul>
-      </div>
+<div className={`mobile-menu ${open ? "open" : ""}`}>
+  <div className="close-icon" onClick={handleClose}>
+    <i className="fa-solid fa-xmark icon"></i>
+  </div>
+
+  <ul>
+    <li ><NavLink className="nav-link mobile-link" to="/" onClick={handleClose}>Home</NavLink></li>
+    <li><NavLink  className="nav-link mobile-link" to="/about" onClick={handleClose}>About</NavLink></li>
+    <li ><NavLink className="nav-link mobile-link" to="/skills" onClick={handleClose}>Skills</NavLink></li>
+    <li ><NavLink className="nav-link mobile-link" to="/projects" onClick={handleClose}>Projects</NavLink></li>
+    <li ><NavLink className="nav-link mobile-link" to="/contact" onClick={handleClose}>Contact</NavLink></li>
+  </ul>
+</div>
+
 
     </nav>
   )
