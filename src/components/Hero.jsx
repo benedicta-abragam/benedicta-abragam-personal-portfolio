5ed2de113797aa8f9f4bd99750e8843b1f5548e8
benedicta-abragam <benedictaabraham079@gmail.com>
2026-02-20 15:42:34 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,16 +6,17 @@ import "../styles/Hero.css"
 function Hero() {
   return (
     <section id="home"
-      className="min-h-screen flex items-center bg-[#02131c] px-6 md:px-20 lg:px-24">
+       className="min-h-screen flex items-start md:items-center bg-[#02131c] px-6 md:px-20 lg:px-24 pt-20 md:pt-12">
       <div className="w-full max-w-7xl mx-auto flex flex-col md:flex-row justify-around items-center">
         {/* LEFT CONTENT */}
         <div className=" md:space-y-6">
-          <p className="text-gray-400 text-base md:text-lg mb-1 md:mb-0">
+          <p className="text-gray-400 text-base md:text-lg md:mb-8">
             Hi there, I'm
           </p>
           {/* Name + Profile */}
-          <div className="flex items-center justify-between md:block mb-2 md:mb-0">            {/* Name */}
-            <h1 className="w-[65%] sm:text-4xl md:text-5xl font-bold neon-texts">
+          <div className="flex items-center justify-between md:block md:mb-0">           
+             {/* Name */}
+            <h1 className="w-[65%] sm:text-4xl md:text-5xl font-bold  md:mb-8 neon-texts">
               Benedicta
             </h1>
 
@@ -34,7 +35,7 @@ function Hero() {
           </div>
 
           {/* Typing */}
-          <div className="sm:text-2xl md:text-3xl font-semibold text-[#00ffd5] mb-2 md:mb-0 min-h-[28px]">            <TypeAnimation
+          <div className="sm:text-2xl md:text-3xl font-semibold text-[#00ffd5] md:mb-8 min-h-[28px]">            <TypeAnimation
             sequence={[
               "Frontend Developer",
               2000,
@@ -49,11 +50,12 @@ function Hero() {
           </div>
 
           {/* Professional Fresher Description */}
-          <p className="text-gray-400 leading-relaxed max-w-xl text-sm md:text-base mt-2 md:mt-0">            Passionate Full Stack Developer with strong skills in React, Node.js, and MongoDB, eager to create scalable and performance-driven web solutions.
+          <p className="text-gray-400 leading-relaxed max-w-xl text-sm md:text-base mt-5 md:mt-0">         
+               Passionate Full Stack Developer with strong skills in React, Node.js, and MongoDB, eager to create scalable and performance-driven web solutions.
           </p>
 
           {/* Buttons */}
-          <div className="flex flex-wrap gap-4 pt-2">
+          <div className="flex flex-wrap gap-5 pt-5 text-center">
 
             <NavLink to="/contact">
               <button className="px-6 py-3 bg-[#00ffd5] text-black rounded-full font-semibold hover:bg-[#0b1f2a] hover:text-[#00ffd5] transition">
